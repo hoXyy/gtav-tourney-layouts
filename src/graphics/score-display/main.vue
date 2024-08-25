63a9b5fe6ba1295d146ad8eb09ec7c376048dce0
Stormra1d <lennschlott@gmail.com>
2024-08-25 12:19:50 +0200

    feat: add base for score-display

---
+++ src/graphics/score-display/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <TopBar />
+    <MatchInfo />
+    <Omnibar />
+  </template>
+  
+  <script setup lang="ts">
+    import Omnibar from '../components/Omnibar.vue';
+    import MatchInfo from '../components/MatchInfo.vue';
+    import TopBar from '../components/TopBar.vue';
+
+    //TODO this is just the base without implementation, if we ever expand this, make this an actual component 
+
+  </script>
+  
+  <style>
+    @import url('../css/base.css');
+  </style>
